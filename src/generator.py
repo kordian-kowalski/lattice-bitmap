eef0161cb26b08b5de91e8a27c60b90a4e3fc0f4
Kordian Kowalski <kordian.kowalski@gmail.com>
2020-07-11 15:18:18 +0200

    fixed offset calculation

--- src/generator.py
+++ src/generator.py
@@ -4,8 +4,8 @@ from PIL import Image
 def generate(width, height, wallsize, gapsize, border):
     total_size = wallsize + gapsize
 
-    h_offset = (width % total_size) / 2 + 1
-    v_offset = (height % total_size) / 2 + 1
+    h_offset = (width % total_size) / 2 - 1
+    v_offset = (height % total_size) / 2 - 1
 
     im = Image.new('1', (width, height), color=0)
     px = im.load()
@@ -16,7 +16,7 @@ def generate(width, height, wallsize, gapsize, border):
                        or y < border \
                        or x >= width - border \
                        or y >= height - border \
-                       or ((x + h_offset) % total_size < wallsize) \
-                       or ((y + v_offset) % total_size < wallsize)
+                       or ((x - h_offset) % total_size < wallsize) \
+                       or ((y - v_offset) % total_size < wallsize)
 
     return im
